5a19c9a388cce91adb75005e0d02592c34937735
Ricardo <ricardoparedesg16@gmail.com>
2023-05-02 13:12:25 -0600

    Primeros avances de proyecto y copia de proyecto

---
+++ proyecto copy/function.js
@@ -0,0 +1,11 @@
+function buscarPelicula() {
+    var input = document.getElementById("input-busqueda").value;
+    // Lógica de búsqueda de películas aquí
+    // Puedes utilizar AJAX para realizar la búsqueda en una base de datos o API externa
+}
+
+  // Función para ordenar los resultados de búsqueda
+function ordenarPor(orden) {
+    // Lógica de ordenamiento de películas aquí
+    // Puedes utilizar AJAX para obtener los resultados ordenados desde una base de datos o API externa
+}
